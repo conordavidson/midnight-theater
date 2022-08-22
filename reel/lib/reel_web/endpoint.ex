049be46a8288954a4e200a3e2fd2e3bbc6310d29
Conor Davidson <conor@conordavidson.com>
2022-08-21 21:38:22 -0400

    Allow new domains in CORS policy

--- reel/lib/reel_web/endpoint.ex
+++ reel/lib/reel_web/endpoint.ex
@@ -10,43 +10,51 @@ defmodule ReelWeb.Endpoint do
     signing_salt: "KZCo+ISw"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :reel,
     gzip: false,
     only: ~w(assets fonts images favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :reel
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :reel)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Corsica,
-    origins: ["http://localhost:3000", "https://midnight.theater"],
+  plug(Corsica,
+    origins: [
+      "http://localhost:3000",
+      ~r/^https:\/\/(.*\.)?midnight\.theater$/,
+      ~r/^https:\/\/(.*\.)?vercel\.app$/
+    ],
     allow_headers: ["content-type", "accept"]
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug ReelWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(ReelWeb.Router)
 end
