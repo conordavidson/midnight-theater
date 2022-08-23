1e9f412e61573c0407c2046c894f1b7aa880a921
Conor Davidson <conor@conordavidson.com>
2022-08-23 08:14:38 -0400

    Add axe core

--- theater/pages/_app.tsx
+++ theater/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
+import React from 'react';
 import type { AppProps } from 'next/app';
 
 import * as Theater from 'lib/theater';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  const ReactDOM = require('react-dom');
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
